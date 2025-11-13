159c98edef7d3bf8bf37a708abbcaef3c53b3aa1
Donald Cameron <donald.cameron@qa.com>
2025-11-13 12:37:03 +0000

    added adv.py

---
+++ calc/app/adv.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# Author: DCameron
+# Description: This module defines several advanced calc functions
+""" 
+    Advanced Calc Module with power, modulus and sqrt functions
+"""
+import sys
+
+def power(x, z):
+    """ Return Exponent of x to power z """
+    return float(x**z)
+
+def mod(x, z):
+    """ Return Remainder after integer division of x by z """
+    return float(x % z)
+
+def sqrt(x):
+    """ Return Square root of x """
+    return float(x**0.5)
+
+print("----------- Advanced Calc Examples ----------")
+print(f"9**8 = {power(9, 8)}")
+print(f"90 % 80 = {mod(90, 80)}")
+print(f"\N{square root}90 = {sqrt(90)}")
+
+
+sys.exit(0)
