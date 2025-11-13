00050ee41ccf05ba159ae2bde166f7ead29a39e3
Donald Cameron <donald.cameron@qa.com>
2025-11-13 16:58:43 +0000

    Updated docstrings

--- calc/app/basic.py
+++ calc/app/basic.py
@@ -7,21 +7,32 @@
 import sys
 
 def add(*args):
-    """ Return SUM of all parameters as a float """
+    """ Return SUM of all parameters as a float
+    >>> add(4, 3, 2, 1)
+    10.0
+    >>> add(20, 30)
+    50.0
+    """
     sum = 0
     for num in args:
         sum += num
     return float(sum)
 
 def mul(*args):
-    """ Return PRODUCT of all parameters as a float """
+    """ Return PRODUCT of all parameters as a float
+    >>> mul(4, 3)
+    12.0
+    """
     total = 1
     for num in args:
         total *= num
     return float(total)
 
 def div(x, z):
-    """ Return QUOTIENT of x divided by z to 3 decimal places """
+    """ Return QUOTIENT of x divided by z to 3 decimal places
+    >>> div(4, 3)
+    1.333
+    """
     return round(x/z, 3)
 
 def main():
@@ -35,5 +46,7 @@ def main():
 if __name__ == "__main__":
     # Execute ONLY if ran directly as a program
     # Ignore if imported as a module
+    import doctest
+    doctest.testmod()
     main()
     sys.exit(0)
